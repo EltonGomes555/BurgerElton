003cb964c774628fe61cca883b09c49af1223efe
EltonGomes555 <elton55_55@hotmail.com>
2021-03-04 14:52:32 -0300

    Imagem Hamburguer

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 <h2>Atualizado o Burger</h2>
 <h3>Didática da aula é bem prática</h3>
 <h4>Aula de DevOps</h4>
+<img src="https://www.confeiteiradesucesso.com/wp-content/uploads/2019/03/hamburguergourmet654.jpg" alt="">
